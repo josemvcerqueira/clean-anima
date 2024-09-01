96ee0c39cde2a6e12e3509b361da90432a13abcb
Jose Cerqueira <josecerqueira@protonmail.com>
2024-09-01 07:37:49 +0200

    :wrench: chore: clean up util fns

--- sdk/src/anima.ts
+++ sdk/src/anima.ts
@@ -10,8 +10,7 @@ import {
   SUI_CLOCK_OBJECT_ID,
 } from '@mysten/sui/utils';
 import { chunkArray, fetchAllDynamicFields } from '@polymedia/suitcase-core';
-import * as changeCase from 'change-case';
-import { path, pathOr } from 'ramda';
+import { path } from 'ramda';
 import invariant from 'tiny-invariant';
 
 import {
@@ -54,7 +53,13 @@ import {
   UpdateImage,
   UpdateUsernameArgs,
 } from './types';
-import { parseGenesisShopItem, parseKioskItem } from './utils';
+import {
+  parseAvatar,
+  parseGenesisPass,
+  parseGenesisShopItem,
+  parseKioskItem,
+  parseSales,
+} from './utils';
 
 export class AnimaSDK {
   #packages: typeof PACKAGES;
@@ -461,66 +466,7 @@ export class AnimaSDK {
 
     if (!res.data.length) return [];
 
-    return res.data.map((elem) => ({
-      objectId: pathOr('', ['data', 'objectId'], elem),
-      version: pathOr('', ['data', 'version'], elem),
-      digest: pathOr('', ['data', 'digest'], elem),
-      type: pathOr('', ['data', 'type'], elem),
-      avatarModel: pathOr(
-        '',
-        ['data', 'content', 'fields', 'avatar_model'],
-        elem
-      ),
-      avatarTexture: pathOr(
-        '',
-        ['data', 'content', 'fields', 'avatar_texture'],
-        elem
-      ),
-      edition: pathOr('', ['data', 'content', 'fields', 'edition'], elem),
-      imageUrl: pathOr('', ['data', 'content', 'fields', 'image_url'], elem),
-      attributes: pathOr(
-        [] as Record<string, string>[],
-        ['data', 'content', 'fields', 'attributes', 'fields', 'contents'],
-        elem
-      ).reduce(
-        (acc, elem) => {
-          const data = pathOr({} as Record<string, string>, ['fields'], elem);
-          return {
-            ...acc,
-            [changeCase.camelCase(data.key)]: data.value,
-          };
-        },
-        {} as Record<string, string>
-      ),
-      misc: pathOr(
-        [] as Record<string, string>[],
-        ['data', 'content', 'fields', 'misc', 'fields', 'contents'],
-        elem
-      ).reduce(
-        (acc, elem) => {
-          const data = pathOr({} as Record<string, string>, ['fields'], elem);
-          return {
-            ...acc,
-            [changeCase.camelCase(data.key)]: data.value,
-          };
-        },
-        {} as Record<string, string>
-      ),
-      attributesHash: pathOr(
-        [] as Record<string, string>[],
-        ['data', 'content', 'fields', 'attributes_hash', 'fields', 'contents'],
-        elem
-      ).reduce(
-        (acc, elem) => {
-          const data = pathOr({} as Record<string, string>, ['fields'], elem);
-          return {
-            ...acc,
-            [changeCase.camelCase(data.key)]: data.value,
-          };
-        },
-        {} as Record<string, string>
-      ),
-    }));
+    return res.data.map((elem) => parseAvatar(elem));
   }
 
   newAvatar({ tx = new Transaction(), recipient }: NewAvatarArgs) {
@@ -695,20 +641,7 @@ export class AnimaSDK {
 
     if (!passes.data.length) return [];
 
-    return passes.data.map((elem) => ({
-      objectId: pathOr('', ['data', 'content', 'fields', 'id', 'id'], elem),
-      phase: BigInt(pathOr(0n, ['data', 'content', 'fields', 'phase'], elem)),
-      name: pathOr('', ['data', 'content', 'fields', 'name'], elem),
-      imageUrl: pathOr('', ['data', 'content', 'fields', 'image_url'], elem),
-      description: pathOr(
-        '',
-        ['data', 'content', 'fields', 'description'],
-        elem
-      ),
-      type: pathOr('', ['data', 'type'], elem),
-      digest: pathOr('', ['data', 'digest'], elem),
-      version: pathOr('', ['data', 'version'], elem),
-    }));
+    return passes.data.map((elem) => parseGenesisPass(elem));
   }
 
   async getSales() {
@@ -717,18 +650,7 @@ export class AnimaSDK {
       options: { showContent: true },
     });
 
-    return {
-      objectId: pathOr('', ['data', 'content', 'fields', 'id', 'id'], data),
-      active: pathOr(false, ['data', 'content', 'fields', 'active'], data),
-      prices: pathOr([], ['data', 'content', 'fields', 'prices'], data),
-      maxMints: pathOr([], ['data', 'content', 'fields', 'max_mints'], data),
-      dropsLeft: BigInt(
-        pathOr(0n, ['data', 'content', 'fields', 'drops_left'], data)
-      ),
-      type: pathOr('', ['data', 'content', 'type'], data),
-      digest: pathOr('', ['data', 'digest'], data),
-      version: pathOr('', ['data', 'version'], data),
-    };
+    return parseSales(data);
   }
 
   /**
--- sdk/src/utils.ts
+++ sdk/src/utils.ts
@@ -1,5 +1,6 @@
 import { SuiObjectResponse } from '@mysten/sui/client';
 import { toHEX } from '@mysten/sui/utils';
+import * as changeCase from 'change-case';
 import { pathOr } from 'ramda';
 import invariant from 'tiny-invariant';
 
@@ -82,5 +83,96 @@ export const parseKioskItem = (x: SuiObjectResponse) => {
     equipment:
       pathOr('', ['data', 'content', 'fields', 'slot'], x) ||
       pathOr('', ['data', 'content', 'fields', 'type'], x),
+  };
+};
+
+export const parseAvatar = (elem: SuiObjectResponse) => {
+  return {
+    objectId: pathOr('', ['data', 'objectId'], elem),
+    version: pathOr('', ['data', 'version'], elem),
+    digest: pathOr('', ['data', 'digest'], elem),
+    type: pathOr('', ['data', 'type'], elem),
+    avatarModel: pathOr(
+      '',
+      ['data', 'content', 'fields', 'avatar_model'],
+      elem
+    ),
+    avatarTexture: pathOr(
+      '',
+      ['data', 'content', 'fields', 'avatar_texture'],
+      elem
+    ),
+    edition: pathOr('', ['data', 'content', 'fields', 'edition'], elem),
+    imageUrl: pathOr('', ['data', 'content', 'fields', 'image_url'], elem),
+    attributes: pathOr(
+      [] as Record<string, string>[],
+      ['data', 'content', 'fields', 'attributes', 'fields', 'contents'],
+      elem
+    ).reduce(
+      (acc, elem) => {
+        const data = pathOr({} as Record<string, string>, ['fields'], elem);
+        return {
+          ...acc,
+          [changeCase.camelCase(data.key)]: data.value,
+        };
+      },
+      {} as Record<string, string>
+    ),
+    misc: pathOr(
+      [] as Record<string, string>[],
+      ['data', 'content', 'fields', 'misc', 'fields', 'contents'],
+      elem
+    ).reduce(
+      (acc, elem) => {
+        const data = pathOr({} as Record<string, string>, ['fields'], elem);
+        return {
+          ...acc,
+          [changeCase.camelCase(data.key)]: data.value,
+        };
+      },
+      {} as Record<string, string>
+    ),
+    attributesHash: pathOr(
+      [] as Record<string, string>[],
+      ['data', 'content', 'fields', 'attributes_hash', 'fields', 'contents'],
+      elem
+    ).reduce(
+      (acc, elem) => {
+        const data = pathOr({} as Record<string, string>, ['fields'], elem);
+        return {
+          ...acc,
+          [changeCase.camelCase(data.key)]: data.value,
+        };
+      },
+      {} as Record<string, string>
+    ),
+  };
+};
+
+export const parseSales = (data: SuiObjectResponse) => {
+  return {
+    objectId: pathOr('', ['data', 'content', 'fields', 'id', 'id'], data),
+    active: pathOr(false, ['data', 'content', 'fields', 'active'], data),
+    prices: pathOr([], ['data', 'content', 'fields', 'prices'], data),
+    maxMints: pathOr([], ['data', 'content', 'fields', 'max_mints'], data),
+    dropsLeft: BigInt(
+      pathOr(0n, ['data', 'content', 'fields', 'drops_left'], data)
+    ),
+    type: pathOr('', ['data', 'content', 'type'], data),
+    digest: pathOr('', ['data', 'digest'], data),
+    version: pathOr('', ['data', 'version'], data),
+  };
+};
+
+export const parseGenesisPass = (elem: SuiObjectResponse) => {
+  return {
+    objectId: pathOr('', ['data', 'content', 'fields', 'id', 'id'], elem),
+    phase: BigInt(pathOr(0n, ['data', 'content', 'fields', 'phase'], elem)),
+    name: pathOr('', ['data', 'content', 'fields', 'name'], elem),
+    imageUrl: pathOr('', ['data', 'content', 'fields', 'image_url'], elem),
+    description: pathOr('', ['data', 'content', 'fields', 'description'], elem),
+    type: pathOr('', ['data', 'type'], elem),
+    digest: pathOr('', ['data', 'digest'], elem),
+    version: pathOr('', ['data', 'version'], elem),
   };
 };
